271726e7e42fa9f85b207089eb9908b55e71a15b
Muhammad Inam <nudmaufon@gmail.com>
2024-10-22 23:44:19 +0500

    DB-config-fixed

--- src/DB/connectdb.ts
+++ src/DB/connectdb.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const dbConnect = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URL || "");
-    console.log(process.env.MONGODB_URL)
+ 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error}`);
